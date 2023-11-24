faf0788e2eabe35e58579d772c2e917a9487328b
jppalomares <joaopedropalomaresjpjp@gmail.com>
2023-11-24 14:16:13 -0300

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { NotaFiscal } from "./model/NotaFiscal";
+
+const notaF1 = new NotaFiscal (`213578-952`);
+notaF1.pessoaF.setNome(`Joãozin`);
+notaF1.pessoaF.setCpf(`878.530.875-78`);
+console.log(`Nome: ${notaF1.pessoaF.getNome()}`);
+console.log(`CPF: ${notaF1.pessoaF.getCpf()}`);
+console.log(`Nota Fiscal nº: ${notaF1.getNumeroNotaFiscal()}`);
+
+const notaF2v = new NotaFiscal (`104829480-13`);
+notaF2v.pessoaF.setNome(`Paulin`);
+notaF2v.pessoaF.setCpf(`336.344.627-60`);
+console.log(`Nome: ${notaF2v.pessoaF.getNome()}`);
+console.log(`CPF: ${notaF2v.pessoaF.getCpf()}`);
+console.log(`Nota Fiscal nº: ${notaF2v.getNumeroNotaFiscal()}`);
